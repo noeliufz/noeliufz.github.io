6b8382e0b2e81dfe048f31c6e0413f5418eeaa5c
Fangzhou Liu <noeliufz@gmail.com>
2024-04-04 14:51:32 +1100

    update: compress font

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#!/bin/bash
+
+hexo clean
+hexo g
+
+# 构建命令
+command="pyftsubset themes/fluid/source/fonts/MicrosoftJhengHei-full.ttf"
+
+# 递归查找 HTML 文件并作为参数添加到命令中
+find public -type f -name "*.html" -exec printf -- "--text-file=\"%s\" " {} + | xargs $command
