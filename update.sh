c79ad8901fc4ad237e42a9da7d8a51eeafa18b6c
Fangzhou Liu <noeliufz@gmail.com>
2024-05-17 21:42:11 +1000

    update: font

--- update.sh
+++ update.sh
@@ -6,7 +6,8 @@ hexo clean
 hexo g
 
 # 构建命令
-command="pyftsubset source/fonts/MicrosoftJhengHei-full.ttf"
+# command="pyftsubset source/fonts/MicrosoftJhengHei-full.ttf"
+command="pyftsubset source/fonts/KingHwa-full.ttf"
 
 # 递归查找 HTML 文件并作为参数添加到命令中
 
